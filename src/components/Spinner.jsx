b3210519797665c62878db5597acb340357d836a
saurabhmohite8386 <saurabhmohite8386@gmail.com>
2024-08-05 22:22:11 +0530

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import './Spinner.css'
+
+function Spinner(){
+    return (
+        <div>
+            <div className="spinner flex flex-col h-screen justify-center items-center"></div>
+        </div>
+    );
+}
+export default Spinner;
